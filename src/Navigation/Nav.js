24d1dd230b33733dc986228b4d83899b5a90cddf
mnaga35 <nagamahmoud35@gmail.com>
2024-03-03 06:06:10 +0200

    1

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input className="nav-container" type="text" placeholder="Search" />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+            <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+            <AiOutlineShoppingCart className="nav-icons" /> 
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
